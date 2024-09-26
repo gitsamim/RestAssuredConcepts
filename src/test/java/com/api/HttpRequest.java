1910ac91c745db59f7f1b4f46041a315ac62ddab
gitsamim <ahmedsamim54@gmail.com>
2024-09-26 22:23:04 +0530

    second

--- src/test/java/com/api/HttpRequest.java
+++ src/test/java/com/api/HttpRequest.java
@@ -5,6 +5,8 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.HashMap;
+
 /*
 	given() -content type, set cookies, add auth, add param, set headers info ...
 	
@@ -16,7 +18,8 @@ import static org.hamcrest.Matchers.*;
  */
 public class HttpRequest {
 
-	@Test
+	int id=0;
+	//@Test
 	void getUsers()
 	{
 		given()
@@ -24,5 +27,36 @@ public class HttpRequest {
 		.when().get("https://reqres.in/api/users?page=2")
 		
 		.then().statusCode(200).body("page", equalTo(8)).log().all();
+	}
+	
+	@Test(priority=2)
+	void createUsers()
+	{
+		HashMap<String, String> data = new HashMap<String, String>();
+		data.put("name", "samim");
+		data.put("job", "testing");
+		
+		id=given().contentType("application/json").body(data)
+		
+		.when().post("https://reqres.in/api/users").jsonPath().getInt("id");
+		
+		// .then().statusCode(200).log().all();
+		
+		System.out.println("ID IS: " + id); 
+	}
+	
+	@Test(priority=3, dependsOnMethods= {"createUsers"})
+	void updateUsers()
+	{
+		HashMap<String, String> data = new HashMap<String, String>();
+		data.put("name", "sabir");
+		data.put("job", "developer");
+		
+		given().contentType("application/json").body(data)
+		
+		.when().put("https://reqres.in/api/users/"+id)
+		
+		 .then().statusCode(200).log().all();
+		
 	}
 }
